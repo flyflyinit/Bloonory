a095ac093260b7727702736dbb93a21405d9feaf
theophiIe <theomolinatti@gmail.com>
2022-03-21 17:25:15 +0100

    Creation des tables pour la base de donnée + Supp des tables

---
+++ script/sql/delete.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS poche_sang;
+
+DROP TABLE IF EXISTS rendez_vous;
+
+DROP TABLE IF EXISTS information_utilisateur;
+
+DROP TABLE IF EXISTS information_administrateur;
+
+DROP TABLE IF EXISTS commentaire;
+
+DROP TABLE IF EXISTS administrateur;
+
+DROP TABLE IF EXISTS hopital;
+
+DROP TABLE IF EXISTS utilisateur;
