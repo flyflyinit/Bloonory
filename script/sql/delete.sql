c2a8318b2bcf9ea74bfe9c15b04eb617d1074581
theophiIe <theomolinatti@gmail.com>
2022-03-22 22:09:45 +0100

    Modification de la base de donnée supp des tables info_user et info_admin + ajout du mail et du mot de passe directement dans la table admin et user

--- script/sql/delete.sql
+++ script/sql/delete.sql
@@ -2,10 +2,6 @@ DROP TABLE IF EXISTS poche_sang;
 
 DROP TABLE IF EXISTS rendez_vous;
 
-DROP TABLE IF EXISTS information_utilisateur;
-
-DROP TABLE IF EXISTS information_administrateur;
-
 DROP TABLE IF EXISTS commentaire;
 
 DROP TABLE IF EXISTS administrateur;
