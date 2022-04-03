5824960573c2ab024469c505d04649d902bd6ab9
theophiIe <theomolinatti@gmail.com>
2022-04-03 17:11:32 +0200

    Ajout de méthode pour la création de rendez-vous en tant que donateur ou bénéficiaire + ajout de commentaires

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
+// Permet l'utilisation du fichier de configuration .env
 require('dotenv').config()
 
+// Permet d'utiliser les lib
 const express = require("express")
 const bcrypt = require("bcryptjs")
 const session = require('express-session')
 
+// Créer une instance d'express
 let app = express()
 
+// Permet de setup EJS
 app.set('view engine', 'ejs')
 
+// Permet l'utilisation des fichiers dans public et dans script
 app.use('/public', express.static('public'))
 app.use('/script', express.static('script'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Permet de gérer la connexion à la base de donnée
 const pgSession = require('connect-pg-simple')(session)
-
 let pgPool = require('./config/database_pool')
-const User = require("./models/user");
-const Comments = require("./models/comments");
 
+// Permet de setup une session express
 app.use(session({
     store: new pgSession({
         pool : pgPool,
@@ -32,11 +36,12 @@ app.use(session({
 }));
 
 // Les routes
-// Homepage
+// Route vers la page Homepage
 app.get('/', (req, res) => {
     res.redirect('/home')
 })
 
+// Route vers la page Homepage
 app.get('/home', (req, res) => {
     res.render('pages/index')
 })
@@ -57,7 +62,7 @@ app.post('/home',  (req, res) => {
     }
 })
 
-// Comments
+// Route vers la page Comments
 app.get('/comments', async (req, res) => {
     const Comments = require('./models/comments')
     const data = await Comments.find_all_and_info_user()
@@ -71,35 +76,37 @@ app.get('/comments', async (req, res) => {
     res.render('pages/comments', {comments: data, connexion: connexion})
 })
 
+// Permet d'inscrire le commentaire de l'utilisateur connecté dans la base de donnée
 app.post('/comments', async (req, res) => {
     const { comment } = req.body
     const Comments = require('./models/comments')
 
-    const data = await Comments.create(comment, req.session.user.mail_user)
+    await Comments.create(comment, req.session.user.mail_user)
 
     res.redirect('/comments')
 })
 
-// Partners page
+// Route vers la page Partners
 app.get('/partners', (req, res) => {
     res.render('pages/partners')
 })
 
-// FAQs page
+// Route vers la page FAQs
 app.get('/faq', (req, res) => {
     res.render('pages/faq')
 })
 
-// About us page
+// Route vers la page About us
 app.get('/about_us', (req, res) => {
     res.render('pages/about_us')
 })
 
-// Connexion page
+// Route vers la page Login
 app.get('/login', (req, res) => {
     res.render('pages/login')
 })
 
+// Vérifie si l'utilisateur est dans la base de donnée et si le mot de base est correct si c'est le cas créer une session
 app.post('/login', async (req, res) => {
     const {email, password} = req.body
     const User = require('./models/user')
@@ -121,21 +128,24 @@ app.post('/login', async (req, res) => {
     }
 })
 
-// Administration page
+// Route vers la page d'Administration
 app.get('/admin', (req, res) => {
     res.render('pages/login')
 })
 
+// Vérifie si l'admin est dans la base de donnée et si le mot de base est correct si c'est le cas créer une session
 app.post('/admin', (req, res) => {
+    // Pas encore implémenté
     console.log(req.body.email)
     console.log(req.body.password)
 })
 
-// Create an account page
+// Route vers la page Create an account
 app.get('/create_account', (req, res) => {
     res.render('pages/create_account')
 })
 
+// Permet la création d'un compte utilisateur dans la base de donnée
 app.post('/create_account', async (req, res) => {
     let User = require('./models/user')
 
@@ -160,17 +170,51 @@ app.post('/create_account', async (req, res) => {
     }
 })
 
-// Create a donator page
-app.get('/donator', (req, res) => {
-    res.render('pages/donator')
+// Route vers la page Donator
+app.get('/donator', async (req, res) => {
+    let Hospital = require('./models/hospital')
+    const hospitals = await Hospital.find_all()
+
+    res.render('pages/appointment', {title: "Donator", hospitals: hospitals})
 })
 
-// Create a beneficiary page
-app.get('/beneficiary', (req, res) => {
-    res.render('pages/beneficiary')
+// Permet de créer un rendez-vous en tant que donateur dans la base de donnée
+app.post('/donator', async (req, res) => {
+    const { date, start, end } = req.body
+    const address = end.split(',')
+
+    let Hospital = require('./models/hospital')
+    const hospitals = await Hospital.find_by_address(address[0], address[1])
+
+    let Appointment = require('./models/appointment')
+    await Appointment.create(req.session.user.mail_user, hospitals.hopital_id, date, "donnation", "incoming", "no information")
+
+    res.redirect('/home')
 })
 
-// Other page
+// Route vers la page Beneficiary
+app.get('/beneficiary', async (req, res) => {
+    let Hospital = require('./models/hospital')
+    const hospitals = await Hospital.find_all()
+
+    res.render('pages/appointment', {title: "Beneficiary", hospitals: hospitals})
+})
+
+// Permet de créer un rendez-vous en tant que bénéficiaire dans la base de donnée
+app.post('/beneficiary', async (req, res) => {
+    const { date, start, end } = req.body
+    const address = end.split(',')
+
+    let Hospital = require('./models/hospital')
+    const hospitals = await Hospital.find_by_address(address[0], address[1])
+
+    let Appointment = require('./models/appointment')
+    await Appointment.create(req.session.user.mail_user, hospitals.hopital_id, date, "beneficiary", "incoming", "no information")
+
+    res.redirect('/home')
+})
+
+// Permet de rediger toutes les autres url vers la page Home
 app.get('/*', (req, res) => {
     res.redirect('/home')
 })
