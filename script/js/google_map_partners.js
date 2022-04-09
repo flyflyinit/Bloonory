00b10c95fd18b3561ad3c772f80b521bac6f8719
theophiIe <theomolinatti@gmail.com>
2022-04-09 16:26:39 +0200

    Ajout du nom des hopitaux lorsqu'on passe la souris sur un marker

--- script/js/google_map_partners.js
+++ script/js/google_map_partners.js
@@ -14,16 +14,19 @@ function initMap() {
     const data = JSON.parse(document.getElementById('data').textContent)
 
     for (const iterator in data) {
-        geocode({ address: data[iterator]["address"] + ", " + data[iterator]["city"] })
+        geocode({ address: data[iterator]["address"] + ", " + data[iterator]["city"] }, data[iterator]["name"])
     }
 }
 
-function geocode(request) {
+function geocode(request, hospital_name) {
     geocoder
         .geocode(request)
         .then((result) => {
             const { results } = result;
-            marker = new google.maps.Marker({ map, });
+            marker = new google.maps.Marker({
+                map,
+                title: hospital_name,
+            });
             marker.setPosition(results[0].geometry.location);
             marker.setMap(map);
             return results;
